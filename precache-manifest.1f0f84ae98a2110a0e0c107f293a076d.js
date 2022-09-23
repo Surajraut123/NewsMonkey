3c8413c19007ac1a663564ece7e42d932093de43
Suraj Rajendra Raut <rautsuraj345@gmail.com>
2022-09-24 00:40:04 +0530

    Updates

---
+++ precache-manifest.1f0f84ae98a2110a0e0c107f293a076d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7896e18608141798435",
+    "url": "/NewsMonkey/static/css/main.3e97568a.chunk.css"
+  },
+  {
+    "revision": "b7896e18608141798435",
+    "url": "/NewsMonkey/static/js/main.b7896e18.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/NewsMonkey/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "5338b8e9ecc0f415d7b7",
+    "url": "/NewsMonkey/static/js/2.5338b8e9.chunk.js"
+  },
+  {
+    "revision": "889e3d691a04d98b260d",
+    "url": "/NewsMonkey/static/js/runtime~main.889e3d69.js"
+  },
+  {
+    "revision": "5b52466cecf9d2cbf4ceb5b3e647ccf4",
+    "url": "/NewsMonkey/index.html"
+  }
+];
